79e95be89aa569b7567c4ad4a91634cc3503af71
khendersonPC <20666083+khendersonPC@users.noreply.github.com>
2021-04-29 20:45:56 -0600

    added header

--- src/components/search.js
+++ src/components/search.js
@@ -3,10 +3,16 @@ import { Form } from "react-bootstrap"
 
 function Search(props) {
     return (
-        <Form.Group>
-            <Form.Control size="lg" type="text" placeholder="Enter name to search" className="se" onChange={props.handleSearchChange} value={props.searchName}
-          />
-        </Form.Group>
+        <div>
+            <div class="header" style={{padding: "60px", textalign: "center",background: "#1abc9c",color: "white",fontsize: "30px"}}>
+                <h1>Employee Directory</h1>
+                <p>Who the heck is working for us?</p>
+            </div>
+            <Form.Group>
+                <Form.Control size="lg" type="text" placeholder="Enter name to search" className="se" onChange={props.handleSearchChange} value={props.searchName}
+                />
+            </Form.Group>
+        </div>
     )
 }
 
